250e191b2ed37ac41ece66f3fca508d3599422b5
teamcodigodragon <teamcodigodragon@gmail.com>
2026-03-20 13:26:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,37 +3,46 @@ const qrcode = require('qrcode-terminal');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { 
-        args: ['--no-sandbox', '--disable-setuid-sandbox'] 
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
-// ESCRIBE AQUÍ EL NOMBRE EXACTO DE TU GRUPO
 const MI_GRUPO_DESTINO = "Team Codigo Dragon"; 
 
 client.on('qr', qr => {
+    // Esto dibuja el QR en los Logs de Railway
     qrcode.generate(qr, {small: true});
-    console.log('--- COPIA ESTE QR ---');
+    console.log('--- ¡QR LISTO ABAJO! ---');
 });
 
 client.on('ready', () => {
-    console.log('¡Bot conectado con éxito!');
+    console.log('¡BOT ACTIVADO CON ÉXITO EN EL GRUPO!');
 });
 
 client.on('message', async msg => {
-    const chat = await msg.getChat();
-    
-    // Si viene de un grupo y no es el tuyo, lo copia
-    if (chat.isGroup && chat.name !== MI_GRUPO_DESTINO) {
-        const todosLosChats = await client.getChats();
-        const miGrupo = todosLosChats.find(c => c.name === MI_GRUPO_DESTINO);
-        
-        if (miGrupo && msg.body) {
-            const hora = new Date().toLocaleTimeString();
-            const reporte = `🚨 *REPORTE EXTERNO*\n🕒 ${hora}\n👥 *De:* ${chat.name}\n\n${msg.body}`;
-            await client.sendMessage(miGrupo.id._serialized, reporte);
+    try {
+        const chat = await msg.getChat();
+        if (chat.isGroup && chat.name !== MI_GRUPO_DESTINO) {
+            const todosLosChats = await client.getChats();
+            const miGrupo = todosLosChats.find(c => c.name === MI_GRUPO_DESTINO);
+            if (miGrupo && msg.body) {
+                const hora = new Date().toLocaleTimeString();
+                const reporte = `🚨 *REPORTE EXTERNO*\n🕒 ${hora}\n👥 *De:* ${chat.name}\n\n${msg.body}`;
+                await client.sendMessage(miGrupo.id._serialized, reporte);
+            }
         }
-    }
+    } catch (e) { console.log("Error al procesar: ", e); }
 });
 
 client.initialize();
